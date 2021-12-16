e4760dd039f8620d87ba2b81e902a377e383cfe8
Nurru123 <little.emiliya@gmail.com>
2021-12-16 16:30:51 +0300

    заготовочки

---
+++ script.js
@@ -0,0 +1,59 @@
+let svg = `
+<svg class="x" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
+                                <path d="M6 6L14 14" stroke="#C4C4C4"/>
+                                <path d="M6 14L14 6" stroke="#C4C4C4"/>
+                            </svg>
+`
+class Lol {
+    constructor() {
+        this.inputs = document.querySelectorAll('.insertField')
+    }
+
+    createNewInput() {
+        const div = document.createElement('div')
+        div.classList.add('insertField')
+        div.id = `input${this.inputs.length + 1}`
+        const input = document.createElement('input')
+        input.classList.add('input')
+        input.setAttribute('type', 'text')
+        input.setAttribute('name', `input${this.inputs.length + 1}`)
+
+        const deleteDiv = document.createElement('div');
+        deleteDiv.classList.add('delete');
+        deleteDiv.innerHTML = svg;
+        
+
+        div.append(input)
+        div.append(deleteDiv)
+        document.querySelector('.task__inner').append(div)
+        this.refreshInputs()
+    }
+
+    refreshInputs() {
+        this.inputs = document.querySelectorAll('.insertField')
+    }
+
+    deleteItem () {
+        this.inputs.forEach(item => {
+            console.log(item.id)
+        })
+        
+    }
+}
+const lel = new Lol()
+
+function addDelete() {
+    document.querySelectorAll('.delete').forEach(item => {
+        item.addEventListener('click', function(e){
+            e.preventDefault()
+            lel.deleteItem()
+        })
+    })
+}
+addDelete()
+document.querySelector('.add-button').addEventListener('click', function(e){
+    e.preventDefault()
+    lel.createNewInput()
+    addDelete()
+})
