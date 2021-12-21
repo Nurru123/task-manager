e0dbc8d7cd23e9ee068cf4d1080ab3cc077030ee
Nurru123 <little.emiliya@gmail.com>
2021-12-21 11:44:43 +0300

    вроде готово

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-let svg = `
-<svg class="x" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
-                                <path d="M6 6L14 14" stroke="#C4C4C4"/>
-                                <path d="M6 14L14 6" stroke="#C4C4C4"/>
-                            </svg>
-`
-class Lol {
+window.addEventListener('load', () => {
+    init();
+});
+
+let svg;
+let counter;
+
+function init() {
+    svg = `
+    <svg class="x" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4"/>
+    <path d="M6 6L14 14" stroke="#C4C4C4"/>
+    <path d="M6 14L14 6" stroke="#C4C4C4"/>
+    </svg>
+    `
+    const task = new TaskList();
+
+    counter = 1;
+    document.querySelector('.add-button').addEventListener('click', function (e) {
+        e.preventDefault();
+        const inputs = document.querySelectorAll('.input');
+
+        if (inputs[inputs.length - 1].value !== '') {
+            ++counter;
+            task.createNewInput();
+        }
+    })
+    const btnSort = document.querySelector('.sort');
+    btnSort.addEventListener('click', () => {
+        btnSort.classList.toggle('down');
+        btnSort.classList.toggle('up');
+        task.sortTask(
+            btnSort.classList.contains('down') ? 'down' : 'up'
+        );
+    })
+}
+
+
+class TaskList {
     constructor() {
-        this.inputs = document.querySelectorAll('.insertField');
+        this.taskList = [];
+        this.createNewInput();
     }
 
     createNewInput() {
@@ -26,47 +58,34 @@ class Lol {
         div.append(input);
         div.append(deleteDiv);
         document.querySelector('.task__inner').append(div);
-        this.refreshInputs();
+        this.taskList.push(div)
+        deleteDiv.addEventListener('click', () => {
+            this.deleteTask(div)
+        })
     }
 
-    refreshInputs() {
-        this.inputs = document.querySelectorAll('.insertField');
-    }
 
-    deleteItem(id) {
-        if (this.inputs.length > 1) {
-            this.inputs.forEach(item => {
-                if (id === item.firstChild.name) {
-                    item.remove();
-                    this.refreshInputs();
-                }
-            })
+    deleteTask(task) {
+        if (this.taskList.length > 1) {
+            task.remove();
+            const index = this.taskList.indexOf(task);
+            this.taskList.splice(index, 1);
         } else {
-            document.querySelector('.input').value = '';
+            const input = task.querySelector('input');
+            input.value = "";
         }
     }
-}
-const lel = new Lol();
 
-
-function addDelete() {
-    document.querySelectorAll('.delete').forEach(item => {
-        item.addEventListener('click', function (e) {
-            e.preventDefault();
-            lel.deleteItem(item.parentElement.id);
+    sortTask(sortStatus) {
+        this.taskList.sort((a, b) => {
+            if (a.querySelector('input').value > b.querySelector('input').value) {
+                return (sortStatus === 'down') ? 1 : -1;
+            } else if (a.querySelector('input').value < b.querySelector('input').value) {
+                return (sortStatus === 'up') ? 1 : -1;
+            }
         })
-    })
-}
-
-addDelete();
-let counter = 1;
-document.querySelector('.add-button').addEventListener('click', function (e) {
-    e.preventDefault();
-    const inputs = document.querySelectorAll('.input');
-    
-    if (inputs[inputs.length-1].value !== '') {
-        ++counter;
-        lel.createNewInput();
-        addDelete();
+        this.taskList.forEach(task => {
+            document.querySelector('.task__inner').append(task)
+        })
     }
-})
+}
